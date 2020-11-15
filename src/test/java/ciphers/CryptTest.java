46699e44fd07f88011269c7219afa87453c0b4e4
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-11-15 20:28:34 +0200

    created the test for testing the instatiation of the parameters in CRYPT

--- src/test/java/ciphers/CryptTest.java
+++ src/test/java/ciphers/CryptTest.java
@@ -1,7 +1,14 @@
 package ciphers;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CryptTest {
+    @Test
+    public void testIfCrypt_instantiatesCorrectly() {
+        Crypt testCrypt = new Crypt("anne", 4);
+        assertEquals(true, testCrypt instanceof Crypt);
+    }
 
 }
