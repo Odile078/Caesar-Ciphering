154d5d50ef93c8e9eaaa4ba0f540498684ae04d1
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-11-15 20:27:07 +0200

    created getters for the key and the text

--- src/main/java/ciphers/Crypt.java
+++ src/main/java/ciphers/Crypt.java
@@ -4,4 +4,18 @@ public class Crypt {
     private int key;
     private String letters;
 
+    public Crypt( String letters,int key) {
+        this.key = key;
+        this.letters = letters;
+    }
+
+    public int getKey() {
+
+        return key;
+    }
+
+    public String getText() {
+        return letters;
+    }
+
 }
