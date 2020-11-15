c4fad49ef54c62246744930150a067a90e381f9f
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-11-15 21:10:36 +0200

    Added the decrypting logic

--- src/main/java/ciphers/Dcrypt.java
+++ src/main/java/ciphers/Dcrypt.java
@@ -1,4 +1,10 @@
 package ciphers;
 
+import static ciphers.Crypt.encrypting;
+
 public class Dcrypt {
+    public static String decrypting(String letters, int key){
+        return encrypting(letters, 26-key);
+
+    }
 }
