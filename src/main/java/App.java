7992096fd342b2f0f216b5003b81b39223328965
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-11-15 21:05:01 +0200

    Added the main class for the interface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,22 @@
+import ciphers.Crypt;
+import ciphers.Dcrypt;
+
+import java.io.Console;
+
 public class App {
+    public static void main(String[] args) {
+        Console myconsole = System.console();
+        System.out.println("Enter the number to use for encryption:");
+        String encryptNtxt = myconsole.readLine();
+        Integer encryptN = Integer.parseInt(encryptNtxt);
+
+        System.out.println("Enter the text you want encrypted:");
+        String encrypttxt = myconsole.readLine();
+        System.out.println("*********************************");
+        System.out.println("-Encryption");
+
+        System.out.println("Here is the encrypted text: "+ Crypt.encrypting( encrypttxt, encryptN ));
+
+
+    }
 }
