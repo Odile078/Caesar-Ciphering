7c188f47dace4dddf556c1257e540906833b5b3b
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-11-15 20:39:10 +0200

    created the method for encryption

--- src/main/java/ciphers/Crypt.java
+++ src/main/java/ciphers/Crypt.java
@@ -18,4 +18,28 @@ public class Crypt {
         return letters;
     }
 
+    public static String encrypting(String letters, int key ){
+        String msg = "the key must be between 1 and 26!";
+        String txtmsg = "Use letters only";
+
+        StringBuilder cryptedtext = new StringBuilder();
+        for(char letter : letters.toCharArray()){
+            if(Character.isLetter(letter)){
+                if(Character.isUpperCase(letter)){
+                    cryptedtext.append((char) ('A' + (letter - 'A' + key) % 26 ));
+                }
+                else if(Character.isLowerCase(letter)){
+                    cryptedtext.append((char) ('a' + (letter - 'a' + key) % 26 ));
+                }
+
+            }
+
+            else{
+                return txtmsg;
+            }
+        }
+        return cryptedtext.toString();
+    }
+
+
 }
