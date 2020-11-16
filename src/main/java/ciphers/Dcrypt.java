4c6d63d57e63d8310ce1e1080b702c244350f10e
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-11-16 12:53:32 +0200

    Adding test for Dcrypt

--- src/main/java/ciphers/Dcrypt.java
+++ src/main/java/ciphers/Dcrypt.java
@@ -3,6 +3,24 @@ package ciphers;
 import static ciphers.Crypt.encrypting;
 
 public class Dcrypt {
+    private int key;
+    private String letters;
+
+    public Dcrypt( String letters,int key) {
+        this.key = key;
+        this.letters = letters;
+    }
+
+    public int getKey() {
+
+        return key;
+    }
+
+    public String getText() {
+
+        return letters;
+    }
+
     public static String decrypting(String letters, int key){
         return encrypting(letters, 26-key);
 
--- src/test/java/ciphers/DcryptTest.java
+++ src/test/java/ciphers/DcryptTest.java
@@ -1,7 +1,25 @@
 package ciphers;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DcryptTest {
+    @Test
+    public void testIfDecryptinstantiatesCorrectly() {
+        Dcrypt testDcrypt = new Dcrypt("Marie", 4);
+        assertEquals(true, testDcrypt instanceof Dcrypt);
+    }
 
+    @Test
+    public void testIfDcrypt_getskey_4() {
+        Dcrypt testDcrypt = new Dcrypt("Marie", 4);
+        assertEquals(4, testDcrypt.getKey());
+    }
+
+    @Test
+    public void testIfDcrypt_getstext_Marie() {
+        Dcrypt testDcrypt = new Dcrypt("Marie", 4);
+        assertEquals("anne", testDcrypt.getText());
+    }
 }
